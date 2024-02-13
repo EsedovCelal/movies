fcc1a928203e8a2417ff23e568a9d94032b2cbae
Jalal <107685956+EsedovCelal@users.noreply.github.com>
2024-02-13 16:23:21 +0400

    create css

--- src/Components/Home_Characters/Home_Characters.jsx
+++ src/Components/Home_Characters/Home_Characters.jsx
@@ -3,7 +3,6 @@ import Selection from "../Selection.jsx";
 import { status, gender, species } from "../../Helpers/Constants.js";
 import Results from "../Results.jsx";
 import { Box, TextField, Typography } from "@mui/material";
-import "./Home_Characters.css";
 
 export default function HomeCharacters() {
   const [dataFromStatus, setDataFromStatus] = useState(null);
@@ -14,6 +13,8 @@ export default function HomeCharacters() {
   const HomeCharacterFilAndRes = {
     display: "flex",
     justifyContent: "center",
+    maxWidth: "70%",
+    margin: "0 auto",
   };
   const HomeCharacterInputAndText = {
     textAlign: "center",
@@ -24,18 +25,30 @@ export default function HomeCharacters() {
   const HomeCharacterInput = {
     width: "40%",
     marginBottom: "10px",
+    // borderStyle: "solid",
+    // borderColor: "red",
   };
   const HomeCharacterClearFilter = {
     cursor: "pointer",
     textDecoration: "underline",
   };
+  const HomeCharacterFilterText = {
+    textAlign: "center",
+  };
   const HomeCharacterInputFilerAndResults = {};
   const HomeCharacterFilter = {
+    display: "grid",
+    maxHeight: "300px",
+  };
+  const HomeCharacterFilterMain = {
     maxHeight: "300px",
     display: "grid",
     alignContent: "space-between",
+    height: "200px",
   };
-  const HomeCharacterResults = {};
+  const HomeCharacterResults = {
+    // maxWidth: "1200px",
+  };
 
   const dataFromChildForStatus = (data) => {
     setDataFromStatus(data);
@@ -63,6 +76,7 @@ export default function HomeCharacters() {
           <Typography sx={HomeCharacterInputText}>Characters</Typography>
           <Box>
             <TextField
+              placeholder="Search for characters"
               sx={HomeCharacterInput}
               value={dataFromSearch === null ? "" : dataFromSearch}
               onChange={(event) => SetDataFromSearch(event.target.value)}
@@ -71,8 +85,8 @@ export default function HomeCharacters() {
         </Box>
         <Box sx={HomeCharacterFilAndRes}>
           <Box sx={HomeCharacterFilter}>
-            <Box>
-              <Typography>Filters</Typography>
+            <Box sx={HomeCharacterFilterText}>
+              <Typography sx={{ fontWeight: "bold" }}>Filters</Typography>
               <Typography
                 variant="button"
                 sx={HomeCharacterClearFilter}
@@ -81,33 +95,34 @@ export default function HomeCharacters() {
                 Clear Filters
               </Typography>
             </Box>
-            <Selection
-              title="Status"
-              options={status}
-              selectedOption={dataFromStatus}
-              setSelectedOption={dataFromChildForStatus}
-            />
-            <Selection
-              title="Species"
-              options={species}
-              selectedOption={dataFromSpecies}
-              setSelectedOption={dataFromChildForSpecies}
-            />
-            <Selection
-              title="Gender"
-              options={gender}
-              selectedOption={dataFromGender}
-              setSelectedOption={dataFromChildForGender}
-            />
+            <Box sx={HomeCharacterFilterMain}>
+              <Selection
+                title="Status"
+                options={status}
+                selectedOption={dataFromStatus}
+                setSelectedOption={dataFromChildForStatus}
+              />
+              <Selection
+                title="Species"
+                options={species}
+                selectedOption={dataFromSpecies}
+                setSelectedOption={dataFromChildForSpecies}
+              />
+              <Selection
+                title="Gender"
+                options={gender}
+                selectedOption={dataFromGender}
+                setSelectedOption={dataFromChildForGender}
+              />
+            </Box>
           </Box>
-          <Box sx={HomeCharacterResults}>
-            <Results
-              spec={dataFromSpecies}
-              stat={dataFromStatus}
-              gen={dataFromGender}
-              search={dataFromSearch}
-            />
-          </Box>
+          <Results
+            sx={HomeCharacterResults}
+            spec={dataFromSpecies}
+            stat={dataFromStatus}
+            gen={dataFromGender}
+            search={dataFromSearch}
+          />
         </Box>
       </Box>
     </Box>
